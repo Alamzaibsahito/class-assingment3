efe108a95f37a953340f5fe5e853f88da00b1722
Alamzaibsahito <162956522+Alamzaibsahito@users.noreply.github.com>
2024-10-24 08:52:55 +0500

    Projects.tsx

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -6,7 +6,7 @@ import p3 from '../../images/p3.jpg';
 import p4 from '../../images/p4.jpg';
 import p5 from '../../images/p5.jpg';
 
-const Projects= () => {
+const Projects = () => {
     return (
         <section className="py-20 bg-gray-100" id="projects">
             <div className="max-w-6xl mx-auto">
@@ -18,7 +18,7 @@ const Projects= () => {
                         <h3 className="text-xl font-semibold mt-4 text-black">Project 1</h3>
                         <p className="mt-2 text-gray-600">One-Side Form Layout with Icons</p>
                         <p className="mt-2 text-gray-600">
-                            "Designed a sleek footer layout featuring two cards and a prominent photo..."
+                            &quot;Designed a sleek footer layout featuring two cards and a prominent photo...&quot;
                         </p>
                         <a href="#" className="mt-4 inline-block px-4 py-2 bg-pink-400 text-white rounded hover:bg-gray-800">View Project</a>
                     </div>
@@ -26,10 +26,10 @@ const Projects= () => {
                     {/* Project Item 2 */}
                     <div className="max-w-xs bg-white p-5 m-3 shadow-lg rounded-lg transition-transform transform hover:translate-y-1">
                         <Image src={p2} alt="Project 2" className="w-full rounded" />
-                        <h3 className="text-xl font-semibold mt-4  text-black">Project 2</h3>
+                        <h3 className="text-xl font-semibold mt-4 text-black">Project 2</h3>
                         <p className="mt-2 text-gray-600">One-Side Form Layout with Icons</p>
                         <p className="mt-2 text-gray-600">
-                            "Designed a sleek footer layout featuring two cards and a prominent photo..."
+                            &quot;Designed a sleek footer layout featuring two cards and a prominent photo...&quot;
                         </p>
                         <a href="#" className="mt-4 inline-block px-4 py-2 bg-pink-400 text-white rounded hover:bg-gray-800">View Project</a>
                     </div>
@@ -40,7 +40,7 @@ const Projects= () => {
                         <h3 className="text-xl font-semibold mt-4 text-black">Project 3</h3>
                         <p className="mt-2 text-gray-600">One-Side Form Layout with Icons</p>
                         <p className="mt-2 text-gray-600">
-                            "Designed a sleek footer layout featuring two cards and a prominent photo..."
+                            &quot;Designed a sleek footer layout featuring two cards and a prominent photo...&quot;
                         </p>
                         <a href="#" className="mt-4 inline-block px-4 py-2 bg-pink-400 text-white rounded hover:bg-gray-800">View Project</a>
                     </div>
@@ -51,7 +51,7 @@ const Projects= () => {
                         <h3 className="text-xl font-semibold mt-4 text-black">Project 4</h3>
                         <p className="mt-2 text-gray-600">One-Side Form Layout with Icons</p>
                         <p className="mt-2 text-gray-600">
-                            "Designed a sleek footer layout featuring two cards and a prominent photo..."
+                            &quot;Designed a sleek footer layout featuring two cards and a prominent photo...&quot;
                         </p>
                         <a href="#" className="mt-4 inline-block px-4 py-2 bg-pink-400 text-white rounded hover:bg-gray-800">View Project</a>
                     </div>
@@ -62,7 +62,7 @@ const Projects= () => {
                         <h3 className="text-xl font-semibold mt-4 text-black">Project 5</h3>
                         <p className="mt-2 text-gray-600">One-Side Form Layout with Icons</p>
                         <p className="mt-2 text-gray-600">
-                            "Designed a sleek footer layout featuring two cards and a prominent photo..."
+                            &quot;Designed a sleek footer layout featuring two cards and a prominent photo...&quot;
                         </p>
                         <a href="#" className="mt-4 inline-block px-4 py-2 bg-pink-400 text-white rounded hover:bg-gray-800">View Project</a>
                     </div>
